55624a50175c0d66b7ffc61a88d5db37172bc89b
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-06-24 14:46:14 +0100

    # 1.0.2 Lifesteal - General improvements and quality of life

--- src/main/java/me/otisps/oplifesteal/items/LifestealGenerator.java
+++ src/main/java/me/otisps/oplifesteal/items/LifestealGenerator.java
@@ -12,7 +12,7 @@ public class LifestealGenerator {
     public ItemStack getLifeHeart(){
         ItemStack customHeart = new ItemStack(Material.MOOSHROOM_SPAWN_EGG);
         ItemMeta heartMeta = customHeart.getItemMeta();
-        heartMeta.setDisplayName(ChatUtils.hexFormat("&fPlayer Heart"));
+        heartMeta.setDisplayName(ChatUtils.hexFormat("&rPlayer Heart"));
         customHeart.setItemMeta(heartMeta);
         return customHeart;
     }
@@ -20,8 +20,8 @@ public class LifestealGenerator {
     public ItemStack getReviveBeacon(){
         ItemStack customBeacon = new ItemStack(Material.BEACON);
         ItemMeta beaconMeta = customBeacon.getItemMeta();
-        beaconMeta.setLore(Collections.singletonList(ChatUtils.hexFormat("&fRename me before placing!")));
-        beaconMeta.setDisplayName(ChatUtils.hexFormat("&fRevive Beacon"));
+        beaconMeta.setLore(Collections.singletonList(ChatUtils.hexFormat("&rRename me before placing!")));
+        beaconMeta.setDisplayName(ChatUtils.hexFormat("&rRevive Beacon"));
         customBeacon.setItemMeta(beaconMeta);
         return customBeacon;
     }
